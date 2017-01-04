f52ab7b131ffdd29bbbb7afe042f3012fb5c6894
Jeremy Zagorski <jmzagorski@gmail.com>
2017-01-04 14:00:20 -0500

    inital commit, add proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+assignProxy() {
+  PROXY_ENV="http_proxy https_proxy ftp_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY"
+
+  for envar in $PROXY_ENV
+  do
+    export $envar=$1
+  done
+  for envar in "no_proxy NO_PROXY"
+  do
+    export $envar=$2
+  done
+}
+
+clrProxy() {
+  assignProxy "" # this is what unset does
+}
+
+# check the headers to see if http status is ok
+# output the /dev/null to check exit code ($?)
+# forbidden
+# curl -s --head $proxy_value --connect-timeout 5 | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null
+
+ping -q -c 1 -W 4 $proxy_value > /dev/null
+
+# on success export variables
+if [ "$?" = "0" ]; then
+  echo "proxy found setting environment..."
+  proxy_addr="http://$PROXY_WORK:$PROXY_PORT_WORK"
+	no_proxy_value="localhost,127.0.0.1,localaddress,.localdomain.com"
+  assignProxy $proxy_addr $no_proxy_value
+  /usr/bin/git config --global http.proxy $proxy_addr
+else
+  echo "proxy not found unsetting environment"
+  clrProxy
+fi
+
+echo "done"
