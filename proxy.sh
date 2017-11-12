6af8165095b54c7c5197eb7cc0bd5df9d1dc279b
Jeremy Zagorski <jmzagorski@gmail.com>
2017-11-11 22:52:12 -0500

    refactor(proxy): add more tools

--- proxy.sh
+++ proxy.sh
@@ -1,47 +1,78 @@
 #! /bin/bash
 
-assignProxy() {
-  PROXY_ENV="http_proxy https_proxy ftp_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY"
-  NO_PROXY_ENV="no_proxy NO_PROXY"
+##### VARIABLES
+dockerconf="/etc/systemd/system/docker.service.d/proxy.conf"
+dockerhasproxy="$(sed -n "/HTTP_PROXY/p" $dockerconf)"
+PROXY_ENV="http_proxy https_proxy ftp_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY"
+NO_PROXY_ENV="no_proxy NO_PROXY"
+proxy_addr="http://$PROXY_WORK:$PROXY_PORT_WORK"
+no_proxy_addr=$NO_PROXY_WORK
+#####
 
+assignProxy() {
   for envar in $PROXY_ENV
   do
-    export $envar=$1
+    export $envar="$proxy_addr"
   done
   for envar in $NO_PROXY_ENV
   do
-    export $envar=$2
+    export $envar="$no_proxy_addr"
   done
 }
 
-git() {
-  echo "Configuring git..."
+npm() {
+  npmproxy="$(/usr/bin/npm config get proxy)"
 
-  if [[ ${1} ]]; then
-    /usr/bin/git config --global http.proxy $1
-  else
+  if [[ -z "${proxy_addr}" && "${npmproxy}" != "null" ]]; then
+    echo "Removing proxy from npm..."
+    /usr/bin/npm config rm proxy
+    /usr/bin/npm config rm https-proxy
+  elif [[ "${proxy_addr}" && "${npmproxy}" = "null" ]]; then
+    echo "Adding proxy to npm..."
+    /usr/bin/npm config set proxy ${proxy_addr}
+    /usr/bin/npm config set https-proxy ${proxy_addr}
+  fi
+}
+
+git() {
+  gitproxy="$(/usr/bin/git config --global http.proxy)"
+
+  # if the proxy exits and git has it, unset it
+  if [[ -z "${proxy_addr}" && "${gitproxy}" ]]; then
+    echo "Removing proxy from git..."
     /usr/bin/git config --global --unset http.proxy
+  #else if there is a proxy and git does not have it set it
+  elif [[ "${proxy_addr}" && -z "${gitproxy}" ]]; then
+    echo "Adding proxy to git..."
+    /usr/bin/git config --global http.proxy ${proxy_addr}
   fi
 }
 
 docker() {
-  echo "Configuring docker..."
-  proxyconf="/etc/systemd/system/docker.service.d/proxy.conf"
-  hasproxy="$(sed -n "/HTTP_PROXY/p" $proxyconf)"
-
-  if [[ ${1} ]] && [[ ! ${hasproxy} ]]; then
-    sed -i "s|\(Environment=\).*\$|\1\"HTTP_PROXY=$1\" \"NO_PROXY=$2\"|" $proxyconf
+  if [[ "${proxy_addr}" && -z "${dockerhasproxy}" ]]; then
+    echo "Adding proxy to docker..."
+    sed -i "s|\(Environment=\).*\$|\1\"HTTP_PROXY=$proxy_addr\" \"NO_PROXY=$no_proxy_addr\"|" $dockerconf
     systemctl daemon-reload
     systemctl restart docker
-  elif [[ ! ${1} ]] && [[ ${hasproxy} ]]; then
-    sed -i "s/\(Environment=\).*\$/\1/" $proxyconf
+  elif [[ -z "${proxy_addr}" && "${dockerhasproxy}" ]]; then
+    echo "Removing proxy from docker..."
+    sed -i "s/\(Environment=\).*\$/\1/" $dockerconf
     systemctl daemon-reload
     systemctl restart docker
   fi
 }
 
-clrProxy() {
-  assignProxy "" # this is what unset does
+pacman() {
+  pacmanconf="/etc/pacman.conf"
+  xfercmds="$(sed -n "/^XferCommand/p" $pacmanconf)"
+
+  if [[ -z "${proxy_addr}" && "${xfercmds}" ]]; then
+    echo "Removing wget for pacman..."
+    sed -i "s/^XferCommand/#\0/g" ${pacmanconf}
+  elif [[ "${proxy_addr}" && -z "${xfercmds}" ]]; then
+    echo "Using wget for pacman..."
+    sed -i "s/^#\(XferCommand = \/usr\/bin\/wget\)/\1/g" ${pacmanconf}
+  fi
 }
 
 # check the headers to see if http status is ok
@@ -50,21 +81,40 @@ clrProxy() {
 # curl -s --head $proxy_value --connect-timeout 5 | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null
 
 echo "looking for proxy $PROXY_WORK at port ${PROXY_PORT_WORK}..."
-proxy_addr=
 
 ping -q -c 1 -W 4 $PROXY_WORK > /dev/null
 
-# on success export variables
+#while true; do
+  #read -p "Do you want to connect to ${PROXY_WORK}:${PROXY_PORT_WORK}" yn
+  #case $yn in
+    #[Yy]* )
+      #echo "setting environment..."
+      #assignProxy $proxy_addr $no_proxy_addr
+      #break;;
+    #[Nn]* )
+      #echo "unsetting environment"
+      #proxy_addr=
+      #no_proxy_addr=
+      #assignProxy "" # this is what unset does
+      #break;;
+    #* ) echo "Please answer yes or no"
+  #esac
+#done
+
+ #on success export variables
 if [ "$?" = "0" ]; then
   echo "proxy found setting environment..."
-  proxy_addr="http://$PROXY_WORK:$PROXY_PORT_WORK"
-  assignProxy $proxy_addr $NO_PROXY_WORK
+  assignProxy $proxy_addr $no_proxy_addr
 else
   echo "proxy not found unsetting environment"
-  clrProxy
+  proxy_addr=
+  no_proxy_addr=
+  assignProxy "" # this is what unset does
 fi
 
-docker $proxy_addr $NO_PROXY_WORK
-git $proxy_addr
+docker
+git
+pacman
+npm
 
 echo "done"
